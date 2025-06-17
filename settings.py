bb7db596a8ac2eef85efa7b8c696a6eb308a939c
Vansh <148086629+Vansh1945@users.noreply.github.com>
2025-06-17 19:29:11 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-5t#n=zbrymgjdzwele&fs24ulk+li4y=gzehlk=d39_r0!1&fa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost')]
 
 
 # Application definition
